e5d6888002c43348aac898865d30fa40c9faec36
Juan Jiménez <jjuanbj@hotmail.com>
2019-12-05 13:44:25 -0400

    Create TS_Logout_Selenium_Intranet
    This class is used to exit the application.

---
+++ TS_Logout_Selenium_Intranet.java
@@ -0,0 +1,38 @@
+package TestStep;
+
+import library.utility.SeleniumActionController;
+import library.utility.SeleniumUtility;
+import resources.TestStep.TS_Logout_Selenium_IntranetHelper;
+import PageObjects.IntranetWebElements;
+
+/**
+ * Description : Functional Test Script
+ * 
+ * @author jjimenez
+ */
+public class TS_Logout_Selenium_Intranet extends
+		TS_Logout_Selenium_IntranetHelper {
+	/**
+	 * Script Name : <b>TS_Logout_Selenium_Intranet</b> Generated : <b>Dec 12,
+	 * 2019 8:09:54 AM</b> Description : Functional Test Script Original Host :
+	 * WinNT Version 6.3 Build 9600 ()
+	 * 
+	 * @since 2019/12/05
+	 * @author jjimenez
+	 */
+	public void testMain(Object[] args) {
+
+		addDescription("Log out of Intranet");
+
+		if (SeleniumActionController.clickWaitObject(
+				IntranetWebElements.linkLeave_XpathSelector,
+				IntranetWebElements.textboxUser_XpathSelector)) {
+			SeleniumUtility.killDriver();			
+		}else {
+			setScreenshot(getRootTestObject().getScreenSnapshot());
+
+			logTestResult("Verification Point Failed!", false,
+					"The session was not closed correctly. Please check.");		
+		}
+	}
+}
